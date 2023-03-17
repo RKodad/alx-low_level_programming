aacc26df154bb49edd81faac07527bebd2abeaeb
RKodad <rn.kodad@gmail.com>
2023-03-17 10:54:27 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (void)
+{
+	int num;
+	char x;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	for (x = 'a'; x <= 'f'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
