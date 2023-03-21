dfc7222f612fafe3545323ca13e1fde98c93435f
RKodad <rn.kodad@gmail.com>
2023-03-21 12:14:29 -0700

    alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+include "main.h"
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * return: 0
+ */
+void print_alphabet(void)
+{	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+
+}
