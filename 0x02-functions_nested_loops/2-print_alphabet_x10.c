d77e5d1a693550baa314015c123a1b7014c187d0
RKodad <rn.kodad@gmail.com>
2023-03-21 12:40:21 -0700

    prints the alphabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,16 +4,16 @@
  *
  * return: 0
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	char x;
 	int i;
 
-	for (i = 0; i < 10, i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (x = 'a'; x <= 'z', x++)
+		for (x = 'a'; x <= 'z'; x++)
 		{
-			_putchar(x)
+			_putchar(x);
 		}
 		_putchar('\n');
 	}
