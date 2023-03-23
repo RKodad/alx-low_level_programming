5c211086854ac63650e4b9cf287feb5898c6c941
RKodad <rn.kodad@gmail.com>
2023-03-23 15:44:03 -0700

    condition 3 & 5

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - solve fizz buzz from numbers 1 to 100
+ * Description: Multiples of 3, print Fizz. Multiples of 5, print Buzz.
+ * Multiples of both 3 and 5 should print FizzBuzz.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n =1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz");
+		else if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", n);
+
+		if (n != 100)
+			printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
