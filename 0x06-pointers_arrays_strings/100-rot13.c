ce9becd4937c504a408c30f7fb6d453208fa3450
RKodad <rn.kodad@gmail.com>
2023-03-30 14:54:36 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ *
+ * Return: the resulting string
+ */
+
+char *rot13(char *s)
+{
+	int a, n;
+
+	char x[] = "abcdefijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char y[] = "nopqrstuvwxyzabcdefijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (n = 0; x[n] != '\0'; n++)
+		{
+			if (s[a] == x[n])
+			{
+				s[a] = y[n];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
