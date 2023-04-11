2d561d8c5df1097a6aa811c1b717502e2db6be06
RKodad <rn.kodad@gmail.com>
2023-04-11 15:40:04 -0700

    grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - create a 2d integer grid using malloc.
+ * Description: Each element of the grid should be initialized to 0.
+ * @width: int size width
+ * @height: int size height
+ * Return: Pointer to new grid
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int a, b;
+
+	if (width + height < 2 || width < 1 || height < 1)
+		return (NULL);
+
+	grid = malloc(height * sizeof(*grid));
+	if (grid == NULL)
+		return (NULL);
+
+	for (a = 0; a < height; a++)
+	{
+		grid[a] = malloc(width * sizeof(**grid));
+		if (grid[a] == NULL)
+		{
+			for (a--; a >= 0; a--)
+				free(grid[a]);
+			free(grid);
+			return (NULL);
+		}
+		for (b = 0; b < width; b++)
+			grid[a][b] = 0;
+	}
+
+	return (grid);
+}
