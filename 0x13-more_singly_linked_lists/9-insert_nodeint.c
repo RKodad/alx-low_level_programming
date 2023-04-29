a45b562a6cff4ef09a62be9b90766dc76101c135
Ubuntu <ubuntu@ip-172-31-16-90.us-west-2.compute.internal>
2023-04-29 23:42:19 +0000

    solution task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - adds a new node to a linked list
+ *                           at a given position
+ * @head : pointer to the head of the list
+ * @idx  : index of the list where the new node will be added
+ *         (indices start at 0)
+ * @n    : value for the new node to be added
+ * Return: pointer to the new node,
+ *         or NULL - if function fails
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+listint_t *new, *j = *head;
+unsigned int node;
+
+new = malloc(sizeof(listint_t));
+
+if (new == NULL)
+return (NULL);
+
+new->n = n;
+
+if (idx == 0)
+{
+new->next = j;
+*head = new;
+return (new);
+}
+
+for (node = 0; node < (idx - 1); node++)
+{
+if (j == NULL || j->next == NULL)
+return (NULL);
+
+j = j->next;
+}
+
+new->next = j->next;
+j->next = new;
+
+return (new);
+}
